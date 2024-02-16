f7dba85f17fa2aa04a79f969753ed5c01558abc5
1U23CA029DHAKSHA <139437377+1U23CA029DHAKSHA@users.noreply.github.com>
2024-02-16 05:33:41 +0000

    Person.java

--- Person.java
+++ Person.java
@@ -1,33 +1,24 @@
 class Persona{
-        String FirstName;
-            String lastName;
-                int age;
-                    Persona (String fName,String lName,int a) {
-                            FirstName = fName;
-                                    lastName = lName;
-                                            age = a;
-                                                }
-                                                    String getfullname(){
-                                                            return FirstName+" "+lastName;
-                                                                }
-                                                                }
-                                                                class Person{
-                                                                    public static void main(String args[]){
+String FirstName;
+String lastName;
+ int age;
+Persona (String fName,String lName,int a) {
+ FirstName = fName;
+lastName = lName;
+ age = a;
+ }
+String getfullname(){
+return FirstName+" "+lastName;
+     }
+     }
+ class Person{
+     public static void main(String args[]){
                                                                             
-                                                                                    Persona a1 = new Persona("Jhon","Doe",30);
-                                                                                            Persona a2 = new Persona("Alice","Smith",25);
-                                                                                                    System.out.println("Person 1:"+a1.getfullname());
-                                                                                                            System.out.println("Person 2:"+a2.getfullname());
-                                                                                                                    double average = ((a1.age + a2.age)/2.0);
-                                                                                                                            System.out.println("Average Age:"+average);
-                                                                                                                                }
-                                                                                                                                }                               double average = ((a1.age + a2.age)/2.0);
-                                                                                                                            System.out.println("Average Age:"+average);
-                                                                                                                                }
-                                                                                                                                }
-                               double average = ((a1.age + a2.age)/2.0);
-                                                                                                                            System.out.println("Average Age:"+average);
-                                                                                                                                }
-                                                                                                                                }
-}
-}
+     Persona a1 = new Persona("Jhon","Doe",30);
+     Persona a2 = new Persona("Alice","Smith",25);
+    System.out.println("Person 1:"+a1.getfullname());
+ System.out.println("Person 2:"+a2.getfullname());
+ double average = ((a1.age + a2.age)/2.0);
+         System.out.println("Average Age:"+average);
+     }
+ }
